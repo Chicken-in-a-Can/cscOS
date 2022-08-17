18081454843d0ad7b1f6a5dc5c78a74bb959b934
Chicken-in-a-Can <64804772+Chicken-in-a-Can@users.noreply.github.com>
2022-08-17 09:58:40 -0500

    Update profiledef.sh

--- OS/profiledef.sh
+++ OS/profiledef.sh
@@ -22,6 +22,4 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/root/Desktop/Installer.desktop"]="0:0:755"
-  ["/root/.calamares/calamares"]="0:0:755"
 )
