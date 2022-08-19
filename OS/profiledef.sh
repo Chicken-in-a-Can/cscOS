083af8b0a67c7dac57e01ab6a9983a1e99da2e24
Chicken-in-a-Can <ben.stidham1@gmail.com>
2022-08-18 20:46:06 -0500

    Added desktop shortcut for konsole

--- OS/profiledef.sh
+++ OS/profiledef.sh
@@ -23,4 +23,5 @@ file_permissions=(
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/root/.setup"]="0:0:755"
+  ["/root/Desktop/konsole.desktop"]="0:0:755"
 )
