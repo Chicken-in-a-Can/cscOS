b0c43e6dc0c9be6facf150931068822913bdf2fa
Chicken-in-a-Can <64804772+Chicken-in-a-Can@users.noreply.github.com>
2022-08-21 09:01:14 -0500

    Update defaultwall.py

--- OS/airootfs/root/.wallpaper/defaultwall.py
+++ OS/airootfs/root/.wallpaper/defaultwall.py
@@ -1,11 +1,22 @@
 #!/bin/python
 
-f = open("/root/.config/plasma-org.kde.plasma.desktop-appletsrc")
-lines=f.readlines()
-f.close()
-for i in range(len(lines)):
-  if lines[i][0:5] == "Image":
-    lines[i] = "Image=file:///root/.wallpaper/barn_01.jpg\n"
-f=open("/root/.config/plasma-org.kde.plasma.desktop-appletsrc", "w")
-f.writelines(lines)
-f.close()
+import os
+import time
+
+loop = True
+
+while loop:
+  existence = os.path.exists("/root/.config/plasma-org.kde.plasma.desktop-appletsrc")
+  if existence:
+    f = open("/root/.config/plasma-org.kde.plasma.desktop-appletsrc")
+    lines=f.readlines()
+    f.close()
+    for i in range(len(lines)):
+      if lines[i][0:5] == "Image":
+        lines[i] = "Image=file:///root/.wallpaper/barn_01.jpg\n"
+    f=open("/root/.config/plasma-org.kde.plasma.desktop-appletsrc", "w")
+    f.writelines(lines)
+    f.close()
+    loop = False
+  else:
+    time.sleep(.25)
